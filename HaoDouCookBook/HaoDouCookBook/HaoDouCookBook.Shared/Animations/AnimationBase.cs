25e5bb1f9f344b5c809452de93472a142f2f1083
wudixiaop <laiming_5433@163.com>
2014-11-11 18:24:44 +0800

    init commit

---
+++ HaoDouCookBook/HaoDouCookBook/HaoDouCookBook.Shared/Animations/AnimationBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Shared.Animations
+{
+    public abstract class AnimationBase
+    {
+        #region Constructor
+
+        public AnimationBase()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        protected Storyboard _Storyboard = null;
+        protected FrameworkElement AnimationTarget = null;
+
+        #endregion
+
+        #region Animation
+
+        public virtual void Stop()
+        {
+            this._Storyboard.Stop();
+        }
+
+        #endregion
+
+        #region Events
+
+        protected Action<FrameworkElement> AnimationCompleted = null;
+
+        #endregion
+
+    }
+}
