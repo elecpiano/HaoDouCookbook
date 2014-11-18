451174966b47bb01dd93a7eedc1295a0d3830618
wudixiaop <laiming_5433@163.com>
2014-11-18 17:33:18 +0800

    add haodou api url gener

---
+++ HaoDouCookBook/HaoDouCookBook/HaoDouCookBook.Shared/HaoDou/API/ApiUrlHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaoDouCookBook.HaoDou.API
+{
+    public class HaoDouApiUrlHelper
+    {
+        public const string DOMAIN = "";
+        public const string APIPAGE = "index.php";
+        public const string APPID = "7";
+        public const string APPKEY = "37e66171acbfbbb63db742bd3bfc1142";
+        public const string VERSIONCODE = "1";
+        public const string VERSIONNAME = "v1.0.0";
+
+        public static string GetApiUrl(string module, string methodName)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("appid", APPID);
+            parameters.Add("appkey", APPKEY);
+            parameters.Add("vc", VERSIONCODE);
+            parameters.Add("vn", VERSIONNAME);
+            parameters.Add("method", string.Format("{0}.{1}", module, methodName));
+            parameters.Add("deviceid", string.Empty); 
+            parameters.Add("format", "json");
+
+            return string.Format("{0}{1}?{2}", DOMAIN, APIPAGE, CombineURLParameters(parameters));
+        }
+
+        public static string CombineURLParameters(Dictionary<string, string> parameters)
+        {
+            if (parameters == null)
+            {
+                throw new ArgumentException("parameters is null");
+            }
+
+            List<string> paras = new List<string>();
+            foreach (var kv in parameters)
+            {
+                paras.Add(string.Format("{0}={1}", kv.Key, kv.Value));
+            }
+
+            return string.Join("&", paras.ToArray());
+        }
+    }
+}
