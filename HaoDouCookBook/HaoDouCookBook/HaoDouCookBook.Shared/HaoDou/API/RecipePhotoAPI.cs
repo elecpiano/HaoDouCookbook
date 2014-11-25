42978291536a335ed8f57acd9180f7b21f84f21d
wudixiaop <laiming_5433@163.com>
2014-11-26 00:37:21 +0800

    add product page

---
+++ HaoDouCookBook/HaoDouCookBook/HaoDouCookBook.Shared/HaoDou/API/RecipePhotoAPI.cs
@@ -0,0 +1,32 @@
+﻿using HaoDouCookBook.Common;
+using HaoDouCookBook.HaoDou.DataModels.Discovery;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaoDouCookBook.HaoDou.API
+{
+    public class RecipePhotoAPI
+    {
+        public const string MODULE = "Recipephoto";
+
+        public static async Task GetProdcuts(int offset, int limit, int type, int sort, string sign, int? uid, string uuid, Action<ProductPageData> onSuccess, Action<Error> onFail)
+        {
+            string methodName = "getProducts";
+            POSTRequestExecuter postRequest = new POSTRequestExecuter(HaoDouApiUrlHelper.GetApiUrl(MODULE, methodName));
+            postRequest.AddPostData("offset", offset.ToString());
+            postRequest.AddPostData("limit", limit.ToString());
+            postRequest.AddPostData("sort", sort.ToString());
+            postRequest.AddPostData("type", type.ToString());
+            postRequest.AddPostData("sign", sign);
+            postRequest.AddPostData("uuid", uuid);
+            postRequest.AddPostData("uid", uid.HasValue ? uid.Value.ToString() : "0");
+
+            string cacheFileName = string.Format("{0}-{1}-{2}-{3}", methodName, sort.ToString(), type.ToString(), offset.ToString(), limit.ToString());
+
+            HaoDouJsonDataLoader<ProductPageData> loader = new HaoDouJsonDataLoader<ProductPageData>();
+            await loader.LoadAsync(postRequest, true, MODULE, cacheFileName, onSuccess, onFail);
+        }
+    }
+}
---
+++ HaoDouCookBook/HaoDouCookBook/HaoDouCookBook.WindowsPhone/ViewModels/ProductPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Infrastructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaoDouCookBook.ViewModels
+{
+    public class ProductPageViewModel : BindableBase
+    {
+
+    }
+}
