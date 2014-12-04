2d83eb19edccbb9d88ef6bf5003d14bcea563d66
wudixiaop <laiming_5433@163.com>
2014-12-05 00:22:53 +0800

    update

---
+++ HaoDouCookBook/HaoDouCookBook/HaoDouCookBook.Shared/Utility/UIExtersion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Shared.Utility
+{
+    public static class UIExtersion
+    {
+        public static T GetParent<T>(this FrameworkElement element) where T : FrameworkElement
+        {
+            if (element == null)
+            {
+                return null;
+            }
+
+            var parent = VisualTreeHelper.GetParent(element);
+
+            if(parent == null)
+            {
+                return null;
+            }
+
+            while (parent != null)
+            {
+                if (parent is T)
+                {
+                    return parent as T;
+                }
+                else
+                {
+                    parent = VisualTreeHelper.GetParent(parent);
+                }
+            }
+
+            return null;
+        }
+    }
+}
