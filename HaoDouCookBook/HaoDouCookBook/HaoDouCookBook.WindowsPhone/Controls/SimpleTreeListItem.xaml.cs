e093e12b464617ca578ad37734646f2cfed19e98
wudixiaop <laiming_5433@163.com>
2014-12-04 18:24:40 +0800

    shopping list stuff

---
+++ HaoDouCookBook/HaoDouCookBook/HaoDouCookBook.WindowsPhone/Controls/SimpleTreeListItem.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace HaoDouCookBook.Controls
+{
+    public sealed partial class SimpleTreeListItem : UserControl
+    {
+        public static readonly DependencyProperty ItemsPanelProperty = DependencyProperty.Register("ItemsPanel", typeof(ItemsPanelTemplate), typeof(SimpleTreeListItem), new PropertyMetadata(null));
+        public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register("ItemsSource", typeof(object), typeof(SimpleTreeListItem), new PropertyMetadata(null));
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(SimpleTreeListItem), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty ItemDataTemplateProperty = DependencyProperty.Register("ItemDataTemplate", typeof(DataTemplate), typeof(SimpleTreeListItem), new PropertyMetadata(null));
+
+        public ItemsPanelTemplate ItemsPanel
+        {
+            get { return (ItemsPanelTemplate)GetValue(ItemsPanelProperty); }
+            set { SetValue(ItemsPanelProperty, value); }
+        }
+
+        public DataTemplate ItemDataTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemDataTemplateProperty); }
+            set { SetValue(ItemDataTemplateProperty, value); }
+        }
+
+        public object ItemsSource
+        {
+            get { return (object)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public SimpleTreeListItem()
+        {
+            this.InitializeComponent();
+
+            this.stuffsList.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            root.DataContext = this;
+        }
+
+        private void Header_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (this.stuffsList.Visibility == Windows.UI.Xaml.Visibility.Collapsed)
+            {
+                this.stuffsList.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                this.arrowImage.Source = new BitmapImage(new Uri("ms-appx:///../assets/images/up_arrow.png", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                this.stuffsList.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                this.arrowImage.Source = new BitmapImage(new Uri("ms-appx:///../assets/images/down_arrow.png", UriKind.RelativeOrAbsolute));
+            }
+        }
+    }
+}
---
+++ HaoDouCookBook/HaoDouCookBook/HaoDouCookBook.WindowsPhone/ViewModels/ShoppingListPageViewModel.cs
@@ -0,0 +1,74 @@
+﻿using Shared.Infrastructures;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaoDouCookBook.ViewModels
+{
+    public class ShoppingListPageViewModel : BindableBase
+    {
+        public ObservableCollection<StuffCategory> StuffCategories { get; set; }
+
+        public ShoppingListPageViewModel()
+        {
+            StuffCategories = new ObservableCollection<StuffCategory>();
+        }
+    }
+
+
+    public class StuffCategory : BindableBase
+    {
+        private int categoryId;
+
+        public int CategoryId
+        {
+            get { return categoryId; }
+            set { SetProperty<int>(ref categoryId, value); }
+        }
+
+        private string category;
+
+        public string Category
+        {
+            get { return category; }
+            set { SetProperty<string>(ref category, value); }
+        }
+
+        public ObservableCollection<StuffItem> Stuffs { get; set; }
+
+        public StuffCategory()
+        {
+            category = string.Empty;
+            Stuffs = new ObservableCollection<StuffItem>();
+        }
+    }
+
+    public class StuffItem : BindableBase
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { SetProperty<int>(ref id, value); }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty<string>(ref name, value); }
+        }
+
+        public StuffItem()
+        {
+
+        }
+    }
+
+
+}
